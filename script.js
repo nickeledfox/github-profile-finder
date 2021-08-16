4a5c8b950f800f7b3cb73477489b30aafa3eee31
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-08-16 04:08:00 -0700

    Added

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const getUser = async (username) => {
 };
 
 const userCard = (user) => {
-  const card = `
+  let card = `
       <div class="card">
           <div class="accaunt">
             <img
@@ -27,27 +27,39 @@ const userCard = (user) => {
               class="avatar"
             />
             <span class="username">${user.login}</span>
-            <span class="blog">${user.blog}</span>
+            <span class="blog">${user.blog}</span> 
           </div>
           <div class="user-info">
-            <h2>${user.name}</h2>
+            <h2>${user.name == null ? '' : user.name}</h2>
             <div class="info">
-              <div class="location"><i class="fas fa-map-marker-alt"></i>Location: <span>${user.location}</span></div>
-              <div class="company"><i class="fas fa-building"></i>Company: <span>${user.company}</span></div>
+              <div class="location"><i class="fas fa-map-marker-alt"></i>Location: <span>${
+                user.location == null ? 'Earth' : user.location
+              }</span></div>
+              <div class="company"><i class="fas fa-building"></i>Company: <span>${
+                user.company == null ? 'Not mentioned' : user.company
+              }</span></div>
             </div>
             <p>
-            ${user.bio}
+            ${user.bio == null ? '' : user.bio}
             </p>
             <ul>
-              <li><i class="fas fa-user-friends"></i>${user.followers} <strong>Followers</strong></li>
-              <li><i class="fas fa-thumbs-up"></i>${user.following} <strong>Followings</strong></li>
-              <li><i class="fas fa-file-code"></i>${user.public_repos} <strong>Repos</strong></li>
+              <li><i class="fas fa-user-users"></i>${
+                user.followers
+              } <strong>Followers</strong></li>
+              <li><i class="fas fa-friends"></i></i>${
+                user.following
+              } <strong>Followings</strong></li>
+              <li><i class="fas fa-file-code"></i>${
+                user.public_repos
+              } <strong>Repos</strong></li>
             </ul>
             <div id="repos"></div>
           </div>
         </div>
 `;
-  main.innerHTML = card;
+  if (user.value !== null) {
+    main.innerHTML = card;
+  }
 };
 
 const showError = (messege) => {
